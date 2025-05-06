eca68e29e8d3c6360138d774e3fa2c3ff14ea8f6
aakashxyadav <iaakashyadavv@gmail.com>
2025-05-06 15:51:05 +0530

    railway db

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -97,9 +97,7 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://student_management_db_6hez_user:g1FhdiNhfyruMtcpDGq5UGjDQbKnSyTc@dpg-d0ctqr3e5dus73aomg80-a/student_management_db_6hez')
-}
-
+        default='postgresql://postgres:DnXKvITlucxsgpNAMVeJBtXSabPlHWRU@postgres.railway.internal:5432/railway')}
 
 # Password validationpip install dj-database-url
 
