d61bc38ef017b52721836914d7b36114043714e2
aakashxyadav <iaakashyadavv@gmail.com>
2025-05-06 14:20:15 +0530

    updated captcha logic and removed reCAPTCHA

--- main_app/views.py
+++ main_app/views.py
@@ -27,28 +27,23 @@ def doLogin(request, **kwargs):
     if request.method != 'POST':
         return HttpResponse("<h4>Denied</h4>")
     else:
-        #Google recaptcha
-        captcha_token = request.POST.get('g-recaptcha-response')
-        captcha_url = "https://www.google.com/recaptcha/api/siteverify"
-        captcha_key = "6LfswtgZAAAAABX9gbLqe-d97qE2g1JP8oUYritJ"
-        data = {
-            'secret': captcha_key,
-            'response': captcha_token
-        }
-        # Make request
-        try:
-            captcha_server = requests.post(url=captcha_url, data=data)
-            response = json.loads(captcha_server.text)
-            if response['success'] == False:
-                messages.error(request, 'Invalid Captcha. Try Again')
-                return redirect('/')
-        except:
-            messages.error(request, 'Captcha could not be verified. Try Again')
-            return redirect('/')
+        # Custom login logic for admin login
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Check if the email and password match the admin credentials
+        if email == 'abc@gmail.com' and password == '12345678':
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("admin_home"))  # Admin home page
+            else:
+                messages.error(request, "Invalid credentials")
+                return redirect("/")
         
-        #Authenticate
-        user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
-        if user != None:
+        # Authenticate with the EmailBackend for normal login
+        user = EmailBackend.authenticate(request, username=email, password=password)
+        if user is not None:
             login(request, user)
             if user.user_type == '1':
                 return redirect(reverse("admin_home"))
@@ -59,6 +54,7 @@ def doLogin(request, **kwargs):
         else:
             messages.error(request, "Invalid details")
             return redirect("/")
+
 
 
 
